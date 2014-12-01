fa003ee5d0130b55d3059715a59f6c79fb6587ff
Alfredo Levy <alfredo.uy@gmail.com>
2014-12-01 17:48:03 -0200

    added hashmap service

--- hashmap.php
+++ hashmap.php
@@ -12,8 +12,14 @@ URL: https://exporthtmlto-bixsolutions.rhcloud.com/phpmyadmin/
 
 Connection URL: mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/
 */
+$db_host = $_ENV['OPENSHIFT_MYSQL_DB_HOST'];
+$db_user = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
+$db_pass = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
+$db_name = $_ENV['OPENSHIFT_APP_NAME'];
+$db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
 
-$mysqli = new mysqli('mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/', "adminqEJK4jV", "wN4cCHPFzxDC", "hashmap");
+echo "aca si lelga";
+$mysqli = new mysqli($db_host, $db_user, $db_pass, "hashmap");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     http_response_code(503);
