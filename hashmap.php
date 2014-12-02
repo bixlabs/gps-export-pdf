5eb07676e863b57dd9855f5cac6b17326eb163b6
Alfredo Levy <alfredo.uy@gmail.com>
2014-12-02 18:25:39 -0200

    demo 14 - beta, report almost ready

--- hashmap.php
+++ hashmap.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 $db_host = getenv('OPENSHIFT_MYSQL_DB_HOST');
 
 @mysql_connect($db_host, 'adminqEJK4jV', 'wN4cCHPFzxDC');
