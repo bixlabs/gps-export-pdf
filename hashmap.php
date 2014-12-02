a10cc815dc26ef511d6751eb11ce4e9a917e46c9
Alfredo Levy <alfredo.uy@gmail.com>
2014-12-02 18:30:36 -0200

    demo 14 - beta, report almost ready

--- hashmap.php
+++ hashmap.php
@@ -1,7 +1,19 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']) && (   
+       $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'POST' || 
+       $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'DELETE' || 
+       $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'] == 'PUT' )) {
+             header('Access-Control-Allow-Origin: *');
+             header("Access-Control-Allow-Credentials: true"); 
+             header('Access-Control-Allow-Headers: X-Requested-With');
+             header('Access-Control-Allow-Headers: Content-Type');
+             header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); // http://stackoverflow.com/a/7605119/578667
+             header('Access-Control-Max-Age: 86400'); 
+      }
+  exit;
+}
+
 $db_host = getenv('OPENSHIFT_MYSQL_DB_HOST');
 
 @mysql_connect($db_host, 'adminqEJK4jV', 'wN4cCHPFzxDC');
