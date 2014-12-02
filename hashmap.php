68283f8f5bb01d8749a57706d5f360f98a15a522
Alfredo Levy <alfredo.uy@gmail.com>
2014-12-02 16:01:50 -0200

    demo 14 - beta, report almost ready

--- hashmap.php
+++ hashmap.php
@@ -12,13 +12,13 @@ URL: https://exporthtmlto-bixsolutions.rhcloud.com/phpmyadmin/
 
 Connection URL: mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/
 */
-$db_host = $_ENV['OPENSHIFT_MYSQL_DB_HOST'];
+$db_host = getenv('OPENSHIFT_MYSQL_DB_HOST');
 $db_user = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
 $db_pass = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
 $db_name = $_ENV['OPENSHIFT_APP_NAME'];
 $db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
 
-$mysqli = new mysqli($db_host, $db_user, $db_pass, 'hashmap', $db_port);
+$mysqli = new mysqli($db_host, 'adminqEJK4jV', 'wN4cCHPFzxDC', 'hashmap', $db_port);
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
