18669b00338236c13b536d9f0b7392b90de8bd36
Alfredo Levy <alfredo.uy@gmail.com>
2014-12-02 17:31:49 -0200

    demo 14 - beta, report almost ready

--- hashmap.php
+++ hashmap.php
@@ -1,10 +1,6 @@
 <?php
 
 $db_host = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$db_user = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
-$db_pass = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
-$db_name = $_ENV['OPENSHIFT_APP_NAME'];
-$db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
 
 @mysql_connect($db_host, 'adminqEJK4jV', 'wN4cCHPFzxDC');
 mysql_select_db('hashmap') or die('Cant select database'.mysql_error());
@@ -16,7 +12,7 @@ if ($_POST) {
 
 	$result = mysql_query("INSERT INTO hashmap VALUES ('{$key}','{$value}')");
 } else {
-  
+
   $key   = $_REQUEST['key'];
   $result = mysql_query("SELECT value FROM hashmap where thekey = '{$key}'");
   $row = mysql_fetch_array($result, MYSQL_ASSOC);
