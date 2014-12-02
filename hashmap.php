fb6140d7b9c5ed2d9e1b8884be3accc5b191de34
Alfredo Levy <alfredo.uy@gmail.com>
2014-12-02 18:44:55 -0200

    demo 14 - beta, report almost ready

--- hashmap.php
+++ hashmap.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
              header('Access-Control-Allow-Origin: *');
              header("Access-Control-Allow-Credentials: true"); 
              header('Access-Control-Allow-Headers: X-Requested-With');
-             header('Access-Control-Allow-Headers: Content-Type, application-id');
+             header('Access-Control-Allow-Headers: *');
              header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); // http://stackoverflow.com/a/7605119/578667
              header('Access-Control-Max-Age: 86400'); 
       }
@@ -19,7 +19,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Origin: http://localhost');
 header("Access-Control-Allow-Credentials: true"); 
 header('Access-Control-Allow-Headers: X-Requested-With');
-header('Access-Control-Allow-Headers: Content-Type, application-id');
+header('Access-Control-Allow-Headers: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT'); // http://stackoverflow.com/a/7605119/578667
 header('Access-Control-Max-Age: 86400'); 
 
